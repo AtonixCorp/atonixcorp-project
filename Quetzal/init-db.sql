0bb2fbc607cb49657e7f51cf90d4724db6ce57f5
guxegdsa <ofidohub@gmail.com>
2025-06-23 03:35:02 -0400

    commit 1234567890abcdef1234567890abcdef12345678

--- Quetzal/init-db.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(50),
-    email VARCHAR(100)
-);
-
-INSERT INTO users (username, email) VALUES
-('atonixdevmaster', 'source@atonixcorp.com');
